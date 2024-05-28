9d1a71338b9a5ab65d60ddc4845aceff27adc0f1
kagenohito <katoyuki.2000.0101@gmail.com>
2024-05-28 16:44:32 +0900

    マイグレーションとシーダーに記述

--- database/migrations/2024_05_28_055139_create_member__series_table.php
+++ database/migrations/2024_05_28_055139_create_member__series_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('member__series', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('member_id')
+                ->constrained('members');
+            $table->foreignId('series_id')
+                ->constrained('series');
+
             $table->timestamps();
         });
     }
