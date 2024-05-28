015592c88fb8094b495db52d540679a4546d8950
kagenohito <katoyuki.2000.0101@gmail.com>
2024-05-28 14:52:19 +0900

    モデルとマイグレーションとシーダーの作成

---
+++ app/Models/Member_Series.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member_Series extends Model
+{
+    use HasFactory;
+}
